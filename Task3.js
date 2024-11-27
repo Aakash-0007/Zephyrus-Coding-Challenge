345c2139be65cde6ecfb454c0dafd61bc1746ca6
Aakash-0007 <98516665+Aakash-0007@users.noreply.github.com>
2024-11-27 13:16:01 -0500

    Create Task3.js

---
+++ Task3.js
@@ -0,0 +1,13 @@
+// Task 3: Even or Odd (JavaScript)
+// Write a JavaScript function that accepts an integer and returns whether it is even or odd*.
+
+function evenOrOdd(num) {
+  return num % 2 === 0 ? "Even" : "Odd";
+}
+
+/*
+Time Complexity: O(1)
+Space Complexity : O(1)
+
+In this approach if the number is divisble by 2 it is EVEN, if not it is ODD.
+*/
